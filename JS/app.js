4b11b9acb5ff6f8c280f228bf65118639158241a
WojciechBialek <bialekwojtek@o2.pl>
2018-10-25 18:03:59 +0200

    zadania JS obowiazkowe

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-console.log("awdaw");
+
 var li_dropdown = document.querySelectorAll(".li_dropdown");
 
 li_dropdown.forEach(function (el) {
@@ -26,3 +26,42 @@ product.forEach(function (el) {
         this.firstElementChild.style.display = "block";
     });
 });
+
+var changePicturenext = document.querySelector(".changePicturenext");
+var changePictureprev = document.querySelector(".changePicturepref");
+var li = document.querySelectorAll(".firstSection__li");
+
+var num =0;
+console.log(li[num]);
+li[num].classList.add("visible");
+
+
+changePictureprev.addEventListener('click', function(el) {
+    console.log("ASdsad");
+    if(num===0){
+        li[num].classList.remove("visible");
+        num=li.length-1;
+        li[num].classList.add("visible");
+    }else{
+        console.log("Asdasdsadasdas");
+        li[num].classList.remove("visible");
+        num--;
+        li[num].classList.add("visible");
+    }
+
+});
+changePicturenext.addEventListener('click', function(el) {
+    console.log("ASdsad");
+    if(num=== li.length-1){
+        li[num].classList.remove("visible");
+        num=0;
+        li[num].classList.add("visible");
+    }else{
+        console.log("Asdasdsadasdas");
+        li[num].classList.remove("visible");
+        num++;
+        li[num].classList.add("visible");
+    }
+
+});
+
